ba2d2638311374bce26fc0cc80418dcaf707f69d
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-02-28 10:47:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skiv in ett helt tal för att beräkna alntal primtal");
+            int tal = int.Parse(Console.ReadLine());
+            int antal = Metod2(tal);
+            Console.WriteLine("De finns " + antal + " primtal mindre än " + tal + ".");
+        }
+
+        static int Metod2(int tal)
+        {
+            int antal = 0;
+            for (int i = 2; i < tal; i++)
+            {
+                if (Metod(i))
+                {
+                    antal++;
+                }
+            }
+            return antal;
+        }
+
+        static bool Metod(int tal)
+        {
+            int deltal = 0;
+            for (int i = 2; i < tal; i++)
+            {
+                if (tal % i == 0)
+                {
+                    deltal++;
+                }
+            }
+            return deltal == 0;
+        }
+    }
+}
